130b1e43e50458ee8c013d8eb956187e736da46c
blissingout <blissout303@gmail.com>
2025-12-16 07:59:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ document.getElementById("lightbox").onclick = function (e) {
     }
 };
 
-function toggleMenu() {
-    const nav = document.getElementById("nav-menu");
-    nav.classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
