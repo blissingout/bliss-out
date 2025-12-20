758f737d50884a548c41bb862fdf615e2cc29f58
blissingout <blissout303@gmail.com>
2025-12-20 14:53:13 +0530

    Update script.js
    connect both motions of hamburger

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.getElementById("nav-menu");
 
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
+   hamburger.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");
+   });
 });
+
